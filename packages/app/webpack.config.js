5fe63ff0873ee1fdaa32afaa78939c8c075d9a43
skornous <hugo.gennaro@gmail.com>
2019-09-15 23:31:03 +0200

    App architecture & dev env

---
+++ packages/app/webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+
+module.exports = {
+  target: 'web',
+  mode: 'production',
+  context: path.resolve(__dirname, 'src'),
+  entry: './app.ts',
+  devtool: 'cheap-module-source-map',
+  output: {
+    path: path.resolve(__dirname, 'dist/'),
+    library: 'app',
+    libraryTarget: 'umd',
+    filename: 'app.js',
+    globalObject: 'this'
+  },
+  resolve: {
+    extensions: ['.ts', '.js', '.json']
+  },
+  module: {
+    rules: [{
+      test: /\.tsx?$/,
+      loader: 'babel-loader',
+      exclude: /node_modules/
+    }]
+  },
+  devServer: {
+    contentBase: [path.join(__dirname, 'public'), path.join(__dirname, 'assets')],
+    watchContentBase: true,
+    hot: true,
+    port: 3000,
+    historyApiFallback: {
+      index: 'index.html'
+    }
+  }
+};
