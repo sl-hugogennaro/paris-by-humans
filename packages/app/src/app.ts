9e9306d9c00f04f3da29b9b8077430c5571e3850
skornous <hugo.gennaro@gmail.com>
2019-09-16 03:21:12 +0200

    Template 1 is GO

---
+++ packages/app/src/app.ts
@@ -0,0 +1,17 @@
+declare var L;
+import { Map } from "leaflet";
+// import L from "leaflet";
+
+
+const map: Map = L.map('map', {
+  zoomControl: false
+});
+map.setView([48.864716, 2.349014], 11);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+L.control.zoom({
+  position: 'bottomright'
+}).addTo(map);
